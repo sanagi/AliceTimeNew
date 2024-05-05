340dcfa841257ec5c3574adad5902536669856b8
sanagi_nsh <nunua1172@gmail.com>
2024-05-05 16:46:44 +0900

    現状不必要なscript整理

---
+++ AliceTime/Assets/script/GameScript/Manager/SystemManager/PrefabPoolManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+/// <summary>
+/// Prefab Poolクラス。
+/// </summary>
+[AddComponentMenu("Utility/PrefabPoolManager")]
+public class PrefabPoolManager : SingletonMonoBehaviour<PrefabPoolManager>
+{
+    
+}
